70e6d256ea83f71b4f169383db7160b835fe950f
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-09-10 11:19:27 -0400

    environment rb AR nil

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require 'tty-prompt'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-
+ActiveRecord::Base.logger = nil
 require_all 'lib'
 
 
